922b047e94214797d8c4afd48179cb9d13f3ddd5
Jriera <jordiriera81@gmail.com>
2021-09-03 03:12:35 +0200

    Update npm packages and set up server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+//endpoint set up 
+//TODO: refactor to make use of routes
+app.get('/api',(req,res)=>{
+	res.send('Connected to api');
+});
+
+//server initilize on port 3000;
+app.listen(port,()=>{
+	console.log(`server running on localhost:${port}`);
+});
