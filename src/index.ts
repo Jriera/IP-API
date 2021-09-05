106625fe9168e6675556e29f823d3c97ef68064d
Jriera <jordiriera81@gmail.com>
2021-09-05 23:43:11 +0200

    file check and eslint

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ import routes from './routes';
 const app = express();
 const port = 3000;
 
-
 //endpoint set up using a route
 
-app.use('/api',routes);
+app.use('/api', routes);
 
 app.use(express.static('Public'));
 
 //server initilize on port 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
 	console.log(`server running on localhost:${port}`);
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,40 +1,37 @@
 import express from 'express';
 import resizeImage from '../imageProcess';
 import path from 'path';
-import fileCheck from '../fileChecker';
-
+import fs from "fs";
 
 const routes = express.Router();
 
-routes.get('/', (req,res)=>{
-    
-	const filename=(req.query.filename as string);
+routes.get('/', (req, res) => {
+	const filename = req.query.filename as string;
+
+	const height = +(req.query.height as string);
+	const width = +(req.query.width as string);
+	const endName = `assets/thumbnails/${filename} ${width}x${height}.jpg`;
+
+	if(fs.existsSync(endName)){
+		res.sendFile(
+			path.resolve(`./assets/thumbnails/${filename} ${width}x${height}.jpg`)
+		);
+		console.log("serving cached image");
+	}
+
+	else{
+		resizeImage(`${filename}.jpg`, width, height, endName);
+
+	setTimeout(()=>{
+		res.sendFile(
+			path.resolve(`./assets/thumbnails/${filename} ${width}x${height}.jpg`)
+		);
+	},100)
+	console.log("new image generated");
+	}
 	
-	const height=+(req.query.height as string );
-	const width=+(req.query.width as string);
-    const endName =`assets/thumbnails/${filename} ${width}x${height}.jpg`;
-	const promise = new Promise ((resolve,reject)=>{
-		resizeImage(`${filename}.jpg`,width,height,endName);
-		if(fileCheck(endName)) {
-			resolve ('file created successfully');
-		}
-		else {
-			reject('no file was created');}
-
-
-	}).then(()=>{
-       
-		setTimeout(() => {
-			res.sendFile(path.resolve(`./assets/thumbnails/${filename} ${width}x${height}.jpg`));
-		}, 100); 
-        
-	}).catch((error)=>{
-		console.log(error);
-	});
-
-    
-    
-       
+	
+	
 });
 
-export default routes; 
+export default routes;
