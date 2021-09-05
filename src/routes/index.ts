91d9d2a07a8ca64f2f6cceca56b9db4a39bfd7ff
Jriera <jordiriera81@gmail.com>
2021-09-05 23:57:19 +0200

    prettier and eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,37 +1,31 @@
 import express from 'express';
 import resizeImage from '../imageProcess';
 import path from 'path';
-import fs from "fs";
+import fs from 'fs';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
 	const filename = req.query.filename as string;
-
 	const height = +(req.query.height as string);
 	const width = +(req.query.width as string);
 	const endName = `assets/thumbnails/${filename} ${width}x${height}.jpg`;
 
-	if(fs.existsSync(endName)){
+	if (fs.existsSync(endName)) {
 		res.sendFile(
 			path.resolve(`./assets/thumbnails/${filename} ${width}x${height}.jpg`)
 		);
-		console.log("serving cached image");
-	}
-
-	else{
+		console.log('serving cached image');
+	} else {
 		resizeImage(`${filename}.jpg`, width, height, endName);
 
-	setTimeout(()=>{
-		res.sendFile(
-			path.resolve(`./assets/thumbnails/${filename} ${width}x${height}.jpg`)
-		);
-	},100)
-	console.log("new image generated");
+		setTimeout(() => {
+			res.sendFile(
+				path.resolve(`./assets/thumbnails/${filename} ${width}x${height}.jpg`)
+			);
+		}, 100);
+		console.log('new image generated');
 	}
-	
-	
-	
 });
 
 export default routes;
