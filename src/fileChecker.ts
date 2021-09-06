719fe07706d6a90d63178e74958d6cbaace43e1c
Jriera <jordiriera81@gmail.com>
2021-09-06 10:59:33 +0200

    typedoc added

--- src/fileChecker.ts
+++ src/fileChecker.ts
@@ -2,20 +2,11 @@ import fs from 'fs';
 import { promises as fsPromises } from 'fs';
 import { constants } from 'fs';
 
-/* const fileCheck = (path: string) => {
-	try {
-		fsPromises.access(path, constants.F_OK | constants.R_OK);
-		console.log('can access');
-		return true;
-	} catch {
-		console.error('cannot access');
-		return false;
-	}
-}; */
 
 /**
  * @param filePath : string
- * @description Waits for 15 seconds for a file to exist.
+ * @description checks for the duration of 1 second 
+ * for the existence of a file
  */
 const watchFile = (filePath: string): boolean => {
 	const watchStartTime = Date.now();
--- src/imageProcess.ts
+++ src/imageProcess.ts
@@ -1,5 +1,13 @@
 import sharp = require('sharp');
 
+/** Function that resizes an input image and outputs it
+ * with the dimensions specified in the parameters
+ * @param inFile This is the name of the input image
+ * @param widthValue This is the final width of the input image
+ * @param heightValue This is the final height if the input image
+ * @param outFile This is the path (and name) of the transformed image
+*/
+
 const resizeImage = async (
 	inFile: string,
 	widthValue: number,
