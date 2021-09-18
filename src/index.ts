95531c31e2068c8215cc3d5e30a2bedf27296784
Jordi Riera <jordiriera81@gmail.com>
2021-09-18 19:45:19 +0200

    main merged

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import routes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config({path:'.env'});
 
 const app = express();
 const port = 3000;
@@ -13,6 +16,6 @@ app.use(express.static('Public'));
 
 
 //server initilize on port 3000;
-app.listen(process.ENV.PORT || port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`server running on localhost:${port}`);
 });
