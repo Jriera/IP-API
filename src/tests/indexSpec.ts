d4b3f3a430eef54adb19fe7195e64a85bbf81d09
Jordi Riera <jordiriera81@gmail.com>
2021-09-21 11:31:36 +0200

    refactor: Add supertest and remove node-fetch to use with Jasmine

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,18 +1,21 @@
 
-//const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args:T[]));
-import fetch from 'node-fetch';
+import supertest from 'supertest';
+import app from '..';
 
-const statusGetter = async (url: string) => {
-    const serverCode = (await fetch(url)).status;
-    return serverCode;
-};
+
+const coder = async(endpoint:string) =>{
+    const appResponse =await supertest(app).get(endpoint)
+    const code = appResponse.statusCode;
+    return code;
+}
+
 
 describe('Confirm that server and endpoint are reachable', () => {
     it('checks if the server is up', async () => {
-        expect(await statusGetter('http://localhost:300')).toEqual(200);
+        expect(await coder('/')).toEqual(200);
     });
 
     it('checks the endpoint is up', async () => {
-        expect(await statusGetter('http://localhost:3000/api')).toEqual(200);
-    });
+        expect(await coder('/api')).toEqual(200);
+    })
 });
