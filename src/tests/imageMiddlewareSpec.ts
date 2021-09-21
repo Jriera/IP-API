26ad2a0542de7c531cde929196384fe702c53ea1
Jordi Riera <jordiriera81@gmail.com>
2021-09-21 15:44:21 +0200

    test: Add tests for image processing

---
+++ src/tests/imageMiddlewareSpec.ts
@@ -0,0 +1,22 @@
+import supertest from "supertest";
+import  fs from "fs";
+import app
+ from "..";
+
+ 
+ describe('It checks a file is resized and saved',() =>{
+     it('should render an image at the endpoint',async ()=>{
+        const appResponse = await supertest(app).get('/api').query({filename: 'test', height: '100', width:'66' });
+        expect(appResponse.type).toBe('image/jpeg')
+        
+     })
+
+     it('should find an image stored',async ()=>{
+        const appResponse = await supertest(app).get('/api').query({filename: 'test', height: '100', width:'66' });
+         
+        const exists =  fs.existsSync('./assets/thumbnails/test 66x100.jpg');
+         expect(exists).toBe(true);
+         fs.unlinkSync('./assets/thumbnails/test 66x100.jpg');
+     })
+     
+ })
